7075a1a49534941ef2f352032af684ccfa5d41d3
Hope Tambala <hopetambala@outlook.com>
2018-11-29 12:29:40 -0500

    update()

---
+++ models/classes.py
@@ -0,0 +1,11 @@
+class Team():
+   def init(self):
+      pass
+   def __str__(self):
+      pass
+
+class Player():
+   def init(self):
+      pass
+   def __str__(self):
+      pass
--- scripts/scraper.py
+++ scripts/scraper.py
@@ -3,8 +3,9 @@ import json
 from bs4 import BeautifulSoup
 
 
-CACHE_FNAME = 'cacheData.json'
-def loadJson(CACHE_FNAME):
+
+def loadJson(name):
+    CACHE_FNAME = str(name) + '.json'
     try:
         cache_file = open(CACHE_FNAME, 'r')
         cache_contents = cache_file.read()
@@ -14,25 +15,62 @@ def loadJson(CACHE_FNAME):
         CACHE_DICTION = {}
     return CACHE_DICTION
 
-def getWhoScoredData():
-    baseurl = 'https://www.whoscored.com/Statistics'
+def getWhoScoredDataSoup(cacheName, specifier):
+    #baseurl = 'https://www.whoscored.com/'
+    baseurl = 'https://www.transfermarkt.com/'
 
-    CACHE_DICTION = loadJson(CACHE_FNAME)
+    #page_url = baseurl + "/Statistics"
+    page_url = baseurl + str(specifier)
+    header = {'User-Agent': 'hope'}
 
-    unique_ident = baseurl
+    CACHE_DICTION = loadJson(str(cacheName))
+
+    unique_ident = page_url
 
     if unique_ident in CACHE_DICTION:
         page_text = CACHE_DICTION[unique_ident]
     else:
-        page_text = requests.get(baseurl).text #original
+        page_text = requests.get(page_url,headers=header).text #original
         CACHE_DICTION[unique_ident] = page_text
         dumped_json_cache = json.dumps(CACHE_DICTION,indent=4)
-        fw = open(CACHE_FNAME,"w")
+        fw = open(str(cacheName)+'.json',"w")
         fw.write(dumped_json_cache)
         fw.close() # Close the open file
 
-        page_soup = BeautifulSoup(page_text, 'html.parser')
 
-        return page_soup
-    
-whoScoredSoup = getWhoScoredData()
+    page_soup = BeautifulSoup(page_text, 'html.parser')
+    return page_soup
+
+def getStatistics():
+    stats_soup = getWhoScoredDataSoup('premierleague',"premier-league/startseite/wettbewerb/GB1/saison_id/2018")
+    #print(stats_soup.prettify())
+
+
+    #name_div = stats_soup.find_all("a",  class_="team-link")
+
+
+    #gets me something statistics-team-table-detailed
+    divs = stats_soup.find("div", id="yw1").find('tbody')
+
+    rows = divs.find_all('tr')
+
+
+    for row in rows:
+        #Club Name
+        img = row.find('img',alt=True)
+        print(img['alt'])
+
+        
+
+
+    # The different tables
+    '''
+    tableNames = ['summary', 'defensive', 'offensive', 'passing']
+    table = soup.find("div", {"id": "statistics-table-" + tableName }).find("tbody", {"id": "player-table-statistics-body"})
+    '''
+
+    '''
+    for name in name_div:
+        print(name)
+    '''
+getStatistics()
