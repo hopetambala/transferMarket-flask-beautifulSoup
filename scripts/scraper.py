da65f8397db12c3491bc92132b4d437f52b217e2
Hope Tambala <hopetambala@outlook.com>
2018-12-02 08:07:21 -0500

    feat(): print player attributes

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -2,7 +2,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-
+BASEURL = 'https://www.transfermarkt.com/'
 
 def loadJson(name):
     CACHE_FNAME = str(name) + '.json'
@@ -16,7 +16,7 @@ def loadJson(name):
     return CACHE_DICTION
 
 def getWhoScoredDataSoup(cacheName, specifier):
-    baseurl = 'https://www.transfermarkt.com/'
+    baseurl = BASEURL
 
     page_url = baseurl + str(specifier)
     header = {'User-Agent': 'hope'}
@@ -52,10 +52,9 @@ def getTeamsAndPlayers():
     ]
 
     for league in leagues:
-        stats_soup = getWhoScoredDataSoup(league[0],league[1])
-
-        #print(stats_soup.prettify())
-
+        #League Soup
+        #stats_soup = getWhoScoredDataSoup(league[0],league[1])
+        stats_soup = getWhoScoredDataSoup('leagues',league[1])
         #gets me the table
         divs = stats_soup.find("div", id="yw1").find('tbody')
 
@@ -65,12 +64,56 @@ def getTeamsAndPlayers():
         for row in rows:
             #Club Name
             img = row.find('img',alt=True)
-            print(img['alt'])
+            club_name = img['alt']
+            
 
+            #Club URL
             row_of_a= row.find_all('a', href = True,class_='vereinprofil_tooltip')
             first_element = row_of_a[0]
             
             urlExtension = first_element.get('href')
-            print(urlExtension)
+            clubs_soup = getWhoScoredDataSoup('teams',urlExtension[1:]) #removes extra comma
+        
+            team_divs = clubs_soup.find("div", id="yw1").find('tbody')
+
+            team_rows_odd = team_divs.find_all('tr',class_='odd')
+            team_rows_even = team_divs.find_all('tr',class_='even')
+            team_rows = team_rows_even + team_rows_odd
+            
+            for row in team_rows:
+                #print(row.find("td")) //prints some goodies
+                
+                
+                #Player Number
+                print(row.find('div',class_='rn_nummer').text)
+                
+                #Player Name
+                print(row.find('td',class_='hide').text)
+                
+                #Position
+                all_trs = row.find('table',class_='inline-table').find_all('tr')
+                position = all_trs[1].text
+                print(position)
+                
+                #Birthday
+                print(row.find_all('td',class_='zentriert')[1].text) 
+                
+                #Nationality
+                print(row.find('img',class_='flaggenrahmen')['alt'])
+
+                #Club
+                print(club_name)
+
+                
+                #print(row.prettify())
+                
+                
+                
+                print('')
+
+                #print(row.prettify())
+                
+            
+            #print(BASEURL[:29] + urlExtension)
 
 getTeamsAndPlayers()
